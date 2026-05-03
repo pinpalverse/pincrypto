44aef6023f76d0ca42ba2f2c4abf9a1170173780
omar <omar@omouttaki.eu.org>
2026-05-03 10:01:49 +0800

    auto: 2026-05-03_10:01:49

--- pincrypto.h
+++ pincrypto.h
@@ -5,7 +5,7 @@ unsigned long djb2_hash(unsigned char* str){
     unsigned long hash = 5381;
     int c;
 
-    while(c = *str++)
+    while((c = *str++))
         hash = ((hash << 5) + hash) + c;
 
     return hash;
